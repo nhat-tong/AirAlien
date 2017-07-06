775462213c26d5f57f0da76ddb53513363c3c76f
Hoang Quoc Nhat TONG <htong@expertime.com>
2017-07-06 22:30:35 +0200

    add gravatar + toastr notification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def gravatar(user)
+		gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+		"https://www.gravatar.com/avatar/#{gravatar_id}.jpg?d=identicon"
+	end
 end
