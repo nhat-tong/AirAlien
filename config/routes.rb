99bcbd42d7af771012a0eae35bd1e774155beba3
Hoang Quoc Nhat TONG <htong@expertime.com>
2017-07-13 22:39:01 +0200

    add view for room + custom css style

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+   
   root 'pages#home'
 
   devise_for :users, 
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 							 }
   
    resources :users, only: [:show]
+   resources :rooms
 
 end
